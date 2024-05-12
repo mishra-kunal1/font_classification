cb21899dcb3cd5f0212adc17f5d486743fce6fc2
Kunal Mishra <kunalmishra@Kunals-MacBook-Air.local>
2024-05-11 23:47:34 -0400

    first commit

---
+++ config.py
@@ -0,0 +1,15 @@
+# Description: Configuration file for the project
+# parameters for data generation
+num_samples_per_image_train=15
+num_samples_per_iamge_val=3
+width_char_ratio=20
+offset_char =2
+
+# parameters for training
+reshape_height=64
+reshape_width=64
+num_classes=10
+learning_rate=0.003
+batch_size=32
+epochs=10
+eval_batches=30
